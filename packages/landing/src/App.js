60d7c84c63a0956d9fe83cce114313daf9c03639
ahoodiakov <akhudiakov97@gmail.com>
2023-08-26 12:55:05 +0100

    finish fixed

---
+++ packages/landing/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Box } from '@mui/system';
+
+import Landing from './components/Landing';
+import SecondPage from './components/SecondPage';
+
+export default function () {
+  return (
+    <Box>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/secondPage" element={<SecondPage />} />
+            <Route path="/" element={<Landing />} />
+          </Routes>
+        </BrowserRouter>
+    </Box>
+  );
+};
